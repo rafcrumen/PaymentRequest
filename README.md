49483c75441dc887bfdf7221245465b7c97d3060
Rafael Mendoza <rafcrumen@hotmail.com>
2026-05-06 13:08:30 -0600

    readMe-Respuestas

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ ejecutar el comando
 npm install
 npm run dev
 
+
+Preguntas cortas (responder en el README) 
+1. ¿Dónde aplicaste SRP y por qué? 
+Creando una interfaz Repository para que el controler no accese a la base de datos solo consuma el repository y no se vean afectados ambos elementos y sean independientes
+
+2. ¿Cómo aplicaste DIP en tu solución?
+En el program.cs de inicio, se debe injectar las despendecias del dbcontex, asi como todas las que se crean como irepository
+
+3. ¿Qué devuelves en POST con 201 Created y por qué? 
+Por que lo que debe regresar un post
+
+4. ¿Qué base de datos elegiste y por qué? 
+Sql server porque es la que tengo instalada
+
--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0f3bf62b1b7c2c0a77652d9f6322164ed06447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290d75f3cf9d3f9092684709d54f3cebf38e7c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
